b153d1ff25ee75be4570eb5bb69b37025bcc4168
Yurii Voitiuk <voutuk@icloud.com>
2024-02-13 16:30:28 +0200

    task 2 create

--- main.cpp
+++ main.cpp
@@ -1,10 +1,21 @@
-//
-// Created by voutu on 13.02.2024.
-//
-
-#include "iostream"
+#include <iostream>
+#include <cstdio>
 using namespace std;
 
-int main(){
-    cout << "Hello wordl";
+int main()
+{
+    char str[100];
+    cout << "Enter a string(100 char max): ";
+    cin.getline(str, 100);
+
+    /*for (int i = 0; str[i] != '\0'; ++i) {
+        cout << "Cast: " << static_cast<int>(str[i]);
+    }*/
+
+    int num = 0;
+    for (int i = 0; str[i] != '\0'; ++i) {
+        if(str[i] >= 48 && str[i] <= 57) {num++;}
+    }
+    cout << "Num: " << num << endl;
+    return 0;
 }
