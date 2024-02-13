145f91869ad27dd942aa607d0f33208846851663
Yurii Voitiuk <voutuk@icloud.com>
2024-02-13 16:51:11 +0200

    task 2 upl

--- main.cpp
+++ main.cpp
@@ -4,18 +4,38 @@ using namespace std;
 
 int main()
 {
-    char str[100];
-    cout << "Enter a string(100 char max): ";
+    //TASK-1
+    /*char str[100];
+    cout << "Enter string(100 char max): ";
     cin.getline(str, 100);
 
-    /*for (int i = 0; str[i] != '\0'; ++i) {
+    for (int i = 0; str[i] != '\0'; ++i) {
         cout << "Cast: " << static_cast<int>(str[i]);
-    }*/
+    }
 
     int num = 0;
     for (int i = 0; str[i] != '\0'; ++i) {
         if(str[i] >= 48 && str[i] <= 57) {num++;}
     }
     cout << "Num: " << num << endl;
-    return 0;
+    return 0;*/
+
+    //TASK-2
+    char str[100];
+    cout << "Enter a string(100 char max): ";
+    cin.getline(str, 100);
+
+    int max = 0;
+    int tmp = 0;
+
+    for (int i = 0; str[i] != '\0'; ++i) {
+        if (str[i] == 'A' || str[i] == 'a') {tmp ++;}
+        else{
+            if (tmp > max) { max = tmp; }
+            tmp = 0;
+        }
+    }
+
+    if (tmp > max) { max = tmp; } //
+    cout << "Max (A,a): " << max << endl;
 }
