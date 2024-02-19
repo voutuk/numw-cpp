5bb83f45aa44eb0bd4b7db3922c0ca00ed7b59d2
Yurii Voitiuk <voutuk@icloud.com>
2024-02-19 03:11:05 +0200

    Update 12.02-char.cpp

--- 12.02-char.cpp
+++ 12.02-char.cpp
@@ -15,8 +15,8 @@ int main()
     }
 
     int num = 0;
-    for (int i = 0; str[i] != '\0'; ++i) {
-        if(str[i] >= 48 && str[i] <= 57) {num++;}
+    for (int i = 0; str[i] != '\0'; ++i) { /bug
+        if(str[i] >= 48 && str[i] <= 57) {num++;} //?
     }
     cout << "Num: " << num << endl;
     return 0;*/
