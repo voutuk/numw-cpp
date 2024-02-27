ca97d632b9d75ba1e40747afc347987c83b1ad2e
Yurii Voitiuk <voutuk@icloud.com>
2024-02-27 03:03:09 +0200

    FUll upl algoritm 1

--- algoritm1.cpp
+++ algoritm1.cpp
@@ -1,29 +1,24 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-int checkNUM(){
-    int num;
-    cout << "Enter number: ";
+
+double checkNUM(char tmp){
+    double num;
+    cout << "Enter number[" << tmp << "]: ";
+    cin >> num;
     do {
-        cin >> num;
         if (cin.fail()) {
             cin.clear();
             cin.ignore(LLONG_MAX, '\n');
-            cout << "Enter normal data!" << endl;
-            cout << "Enter number: ";
+            cout << "Enter normal data: ";
             cin >> num;
-        } else if (cin.bad()) {
-            cout << "Critical Error" << endl;
-            return -1;
-        } else if (num <= 0){
-            cout << "Enter positive number!" << endl;
-            cout << "Enter number: ";
+        }else if (num == 0){
+            cout << "Enter positive number: ";
             cin >> num;
         }
-    } while (!cin.good() or num <= 0);
+    } while (!cin.good() or num == 0);
     return num;
 }
-
 
 double deg_to_rad(double deg) {
     return deg * M_PI / 180.0;
@@ -41,7 +36,7 @@ int main(){
     cout << "Z1: " << z1 << endl << "Z2: " << z2 << endl;*/
 
     //TASK-2
-    /*int num = checkNUM();
+    /*double num = checkNUM('n');
     // z1 = (1-2sin^2a)/(1+sin2a)
     // z2 = (1-tga)/(1+tga)
     double z1 = (1-2*pow(sin(num), 2))/(1+sin(2*num));
@@ -49,13 +44,20 @@ int main(){
     cout << "Z1: " << z1 << endl << "Z2: " << z2 << endl;*/
 
     //TASK-3
-    /*//x = 3.74*10^-2
-    double x = 3.74*pow(10, -2);
-    //y = -0.825
-    double y = -0.825;
-    //z = 0.16*10^2
-    double z = 0.16*pow(10, 2);
+    double x, y, z;
+    do {
+        x = checkNUM('x');
+        y = checkNUM('y');
+        z = checkNUM('z');
+        cout << "Enter normal data!" << endl;
+    }while(abs(x - 2*y / (1 + pow(x, 2) * pow(y, 2)))==0);
     //  v = (1+sin^2(x+y))/(|x-((2y)/(1+x^2y^2))|)x^|y|+cos^2(arctg((1)/(z)))
     double v = (1 + pow(sin(x+y), 2)) / (abs(x - 2*y / (1 + pow(x, 2) * pow(y, 2)))) * pow(x, abs(y)) + pow(cos(atan(1/z)), 2);
-    cout << "V: " << v << endl;*/
+
+    cout << "V: " << v << endl;
+
+    /*X: 0.0374
+    Y: -0.825
+    Z: 16
+    V: 1.05534*/
 }
